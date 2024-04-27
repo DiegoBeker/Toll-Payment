ade365ae661c7576654c647f3cf02d7a5445a36b
DiegoBeker <diegobecker2007@hotmail.com>
2024-04-27 08:02:11 -0300

    chore: first commit

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Toll_Payment.Services;
+
+namespace Toll_Payment.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class HealthController : ControllerBase
+{
+    private readonly HealthService _healthService;
+
+    public HealthController(HealthService healthService)
+    {
+        _healthService = healthService;
+    }
+
+    [HttpGet]
+    public IActionResult GetHealth()
+    {
+        String message = _healthService.GetHealthMessage();
+        return Ok(message);
+    }
+}
---
+++ Services/HealthService.cs
@@ -0,0 +1,9 @@
+namespace Toll_Payment.Services;
+
+public class HealthService
+{
+    public string GetHealthMessage()
+    {
+        return "I'm alive!";
+    }
+}
