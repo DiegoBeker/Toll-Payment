ff77f2d258e4a6199d872ee15193d0575bea2647
DiegoBeker <diegobecker2007@hotmail.com>
2024-04-28 15:07:14 -0300

    fix: add payment type validation

--- Dtos/CreateTicketDto.cs
+++ Dtos/CreateTicketDto.cs
@@ -7,6 +7,8 @@ public class CreateTicketDto
     [Required(ErrorMessage = "A plate number is required")]
     [RegularExpression(@"^[0-9A-Z]{7}$", ErrorMessage = "The plate number must contain exactly 7 characters and consist of numbers and uppercase letters.")]
     public required string Plate { get; set; }
+    [Required(ErrorMessage = "Payment type is required")]
+    [Range(0, 1, ErrorMessage = "Payment type must be either 0 or 1")]
     public required PaymentType Type { get; set; }
 
     public Ticket ToEntity()
